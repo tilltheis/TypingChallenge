57217f4862415c2ae84b2cb2741cdef88fa05af3
Till Theis <theis.till@gmail.com>
2016-07-03 00:32:34 +0200

    beautiful ui

--- integration.js
+++ integration.js
@@ -2,14 +2,20 @@
 
 (function() {
   var problemEl = document.querySelector("#problem");
+  var problemContainerEl = document.querySelector("#problemContainer");
   var solutionEl = document.querySelector("#solution");
   var restartEl = document.querySelector("#restart");
   var statsEl = document.querySelector("#stats");
   var statsTemplateEl = document.querySelector("#statsTemplate");
 
-  var currentChallenge = null; // set by startGame()
-  var currentGameTimeout = null; // set by startGame()
-  var currentStatsInterval = null; // set by startGame()
+  var currentChallenge = null;
+  var currentGameTimeout = null;
+  var currentStatsInterval = null;
+  var lastWordIndex = null;
+  var rowHeight = null;
+
+  var CorrectRgbColor = utils.stringToRgb("#97b57d");
+  var IncorrectRgbColor = utils.stringToRgb("#c6717e");
 
   var GameDuration = 60000;
   var WordsPerLine = 13;
@@ -50,37 +56,122 @@
   };
 
   var updateGameUi = function(challenge) {
-    var allLines = utils.grouped(challenge.words, WordsPerLine);
-    var activeLineIndex = Math.trunc(challenge.currentWordIndex / WordsPerLine);
-    var visibleLines = allLines.slice(activeLineIndex, activeLineIndex + 2);
+    var isBeforeGameStart = !challenge.isRunning && challenge.startDate === null;
+    var isAfterGameEnd = !challenge.isRunning && challenge.startDate !== null;
 
-    var htmlLines =  visibleLines.map(function(line, lineIndex) {
-      return line.map(function(word, wordIndex) {
-        var globalIndex = (activeLineIndex + lineIndex) * WordsPerLine + wordIndex;
-        var className = "";
-        if (globalIndex === challenge.currentWordIndex) { className += " active"; }
-        if (challenge.correctWordIndexes.indexOf(globalIndex) !== -1) { className += " correct"; }
-        if (challenge.incorrectWordIndexes.indexOf(globalIndex) !== -1) { className += " wrong"; }
-        return "<span class=\"" + className + "\">" + word + "</span>";
+    // create html once and mutate it afterwards to allow for fancy transitions
+    if (isBeforeGameStart) {
+      lastWordIndex = null;
+
+      var lines = utils.grouped(challenge.words, WordsPerLine);
+      var htmlLines = lines.map(function(line, lineIndex) {
+        return line.map(function(word, wordIndex) {
+          var id = "word" + (lineIndex * WordsPerLine + wordIndex);
+          return "<span id=\"" + id + "\">" + word + "</span>";
+        });
       });
-    });
 
-    problemEl.innerHTML = htmlLines.map(function(line) { return line.join(" "); }).join("<br>");
+      problemEl.innerHTML = "<div class=\"line\">" + htmlLines.map(function(line) { return line.join(" "); }).join("</div><div class=\"line\">") + "</div>";
+
+      rowHeight = problemEl.querySelector(".line").clientHeight; // to be able to scroll the lines
+      problemContainerEl.style.height = rowHeight * 2 + "px";
+      problemEl.style.marginTop = "";
+    }
+
+    // word hightlighting
+    challenge.correctWordIndexes.forEach(function(index) {
+      document.getElementById("word" + index).className += " correct";
+    });
+    challenge.incorrectWordIndexes.forEach(function(index) {
+      document.getElementById("word" + index).className += " incorrect";
+    });
+    if (lastWordIndex !== null) {
+      document.getElementById("word" + lastWordIndex).className.replace(/\bactive\b/,'');
+    };
+    document.getElementById("word" + challenge.currentWordIndex).className += " active";
+
+    // new line reached?
+    if (lastWordIndex !== null && lastWordIndex !== challenge.currentWordIndex && challenge.currentWordIndex % WordsPerLine === 0) {
+      var currentMarginTop = problemEl.style.marginTop === "" ? 0 : parseInt(problemEl.style.marginTop);
+      problemEl.style.marginTop = currentMarginTop - rowHeight + "px";
+    }
 
     solutionEl.value = challenge.input;
-    solutionEl.disabled = !challenge.isRunning && challenge.startDate !== null;
+    solutionEl.disabled = isAfterGameEnd;
+
+    if (lastWordIndex !== challenge.currentWordIndex) {
+      lastWordIndex = challenge.currentWordIndex;
+    }
+  };
+
+  // colorize ui according to the player's performance
+  var colorizeStats = function(wpm, isRunning) {
+
+    // more colorful
+    var color =
+      utils.hsvToRgb(
+        utils.hsvColorBetween(
+          utils.rgbToHsv(CorrectRgbColor),
+          utils.rgbToHsv(IncorrectRgbColor),
+          1 - Math.min(1, Math.max(0, wpm - 25) / 50) // <= 25wpm is bad ; >= 75 is good
+        )
+      );
+
+    // less colorful
+    // var color =
+    //   utils.rbgColorBetween(
+    //     CorrectRgbColor,
+    //     IncorrectRgbColor,
+    //     1 - Math.min(1, Math.max(0, wpm - 25) / 50) // <= 25wpm is bad ; >= 75 is good
+    //   );
+
+    var colorfulStats = document.querySelectorAll("p div div, .progress-bar, .stat-value .badge");
+    [].forEach.call(colorfulStats, function(el) {
+      el.style.backgroundColor = "#" + color.r.toString(16) + color.g.toString(16) + color.b.toString(16);
+    });
+
+    var colorfulInputs = document.querySelectorAll("#solution, #restart"); // :focus pseudo class has no effect here
+    [].forEach.call(colorfulInputs, function(el) {
+      el.style.borderColor = "#" + color.r.toString(16) + color.g.toString(16) + color.b.toString(16);
+
+      // complex because we need to handle state w/ and w/o focus manually
+      if (isRunning) {
+        el.onfocus = undefined;
+        el.onblur = undefined;
+        if (el === document.activeElement) {
+          el.style.boxShadow = "inset 0 1px 1px rgba(0, 0, 0, .075), 0 0 8px rgba(" + color.r + ", " + color.g + ", " + color.b + ", .6)";
+        } else {
+          el.style.boxShadow = "";
+        }
+      } else {
+        el.onfocus = function() {
+          el.style.boxShadow = "inset 0 1px 1px rgba(0, 0, 0, .075), 0 0 8px rgba(" + color.r + ", " + color.g + ", " + color.b + ", .6)";
+        };
+        el.onblur = function() {
+          el.style.boxShadow = "";
+        }
+      }
+    });
   };
 
   var updateStatsUi = function(challenge) {
-    var timeLeft = Math.max(0, ((challenge.startDate * 1 + GameDuration) - (new Date() * 1)) / 1000);
-    var wpm = challenge.correctWordIndexes.length / (GameDuration / 1000 - timeLeft) * GameDuration / 1000;
+    var timeLeft = GameDuration / 1000;
+    var wpm = 0;
+
+    if (challenge.startDate !== null) {
+      timeLeft = Math.max(0, ((challenge.startDate * 1 + GameDuration) - (new Date() * 1)) / 1000);
+      wpm = challenge.correctWordIndexes.length / (GameDuration / 1000 - timeLeft) * GameDuration / 1000;
+    }
+
     statsEl.innerHTML = utils.fillTemplate(statsTemplate.innerHTML, {
-      timeLeft: Math.trunc(timeLeft),
-      wpm: Math.trunc(wpm),
+      timeLeft: timeLeft,
+      wpm: wpm,
       keyStrokeCount: challenge.keyStrokeCount,
       correctWordCount: challenge.correctWordIndexes.length,
       incorrectWordCount: challenge.incorrectWordIndexes.length
     });
+
+    colorizeStats(wpm, challenge.isRunning);
   };
 
   restartEl.onclick = startGame;
